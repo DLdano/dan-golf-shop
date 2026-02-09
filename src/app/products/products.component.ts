43939205da993de61810aa2ce33ab3f8668a89ec
Daniel Lee <dlee@craneware.com>
2026-02-09 12:12:06 +0000

    added filtering to products page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,16 +9,38 @@ import { ProductService } from './product-service';
   standalone: true,
   imports: [NgFor, ProductCardComponent],
   templateUrl: './products.component.html',
+  styleUrl: './products.component.scss'
 })
 export class ProductsComponent implements OnInit {
   products: Product[] = [];
+  filteredProducts: Product[] = [];
+  categories: string[] = [];
+  selectedCategory: string = 'All';
 
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe({
-      next: (products) => (this.products = products),
+      next: (products) => {
+        this.products = products;
+        this.filteredProducts = products;
+        this.extractCategories();
+      },
       error: (err) => console.error(err),
     });
+  }
+
+  extractCategories(): void {
+    const uniqueCategories = new Set(this.products.map(p => p.category));
+    this.categories = Array.from(uniqueCategories).sort();
+  }
+
+  filterByCategory(category: string): void {
+    this.selectedCategory = category;
+    if (category === 'All') {
+      this.filteredProducts = this.products;
+    } else {
+      this.filteredProducts = this.products.filter(p => p.category === category);
+    }
   }
 }
